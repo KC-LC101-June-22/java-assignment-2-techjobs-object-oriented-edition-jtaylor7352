fc93f4777d80972aa665b8880fae1238c3e10319
jtaylor7352 <jtaylor7352@gmail.com>
2022-11-21 19:17:25 -0600

    Test 2 Fix

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -25,6 +25,9 @@ public abstract class JobField {
         return value;
     }
 
+    public void setValue(String value) {
+        this.value = value;
+    }
     @Override
     public String toString() {
         if (value.isEmpty()) {
